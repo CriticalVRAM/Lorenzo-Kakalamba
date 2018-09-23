4b45ff95ab521ff9859afb56f90cccc314f781e1
Nani-Sore-0 <urke.radule@gmail.com>
2018-09-23 13:45:59 +0200

    gallery

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         duration: 1200,
     })
 
+
     $(document).ready(function () {
         $('.about__slideshow').slick({
             autoplay: true,
@@ -12,8 +13,28 @@ $(document).ready(function () {
         })
     })
 
+    $('.gallery__interior-selected').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.gallery__interior-view'
+    })
+    $('.gallery__interior-view').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.gallery__interior-selected',
+        centerMode: true,
+        focusOnSelect: true,
+        prevArrow: '<a class="gallery__prev"><svg class="gallery__icon"><use xlink:href="/img/sprite.svg#icon-chevron-left"></use></svg></a>',
+        nextArrow: '<a class="gallery__next"><svg class="gallery__icon"><use xlink:href="/img/sprite.svg#icon-chevron-right"></use></svg></a>',
+    })
+
+
     $('.menu__box').tabslet({
         animation: true,
-        active: 2,
+    })
+    $('.gallery__box').tabslet({
+        animation: true,
     })
 })
