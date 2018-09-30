cafc71cc13b8b8c5a79f8bff2c4d7d35a7f878fa
Nani-Sore-0 <urke.radule@gmail.com>
2018-09-30 11:37:01 +0200

    contact and google maps is trash

--- js/index.js
+++ js/index.js
@@ -70,4 +70,10 @@ $(document).ready(function () {
     }, true)
     activateGallery('interior')
     activateGallery('interior')
+
+    function intiMap() {
+        var uluru = {lat: -25.344, lng: 131.036}
+        var map = new google.maps.Map(document.querySelector('.contact__map'), {zoom: 4, center: uluru})
+        var marker = new google.maps.Marker({position: uluru, map: map})
+    }
 })
