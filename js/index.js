541b5e8010d367fdceeceaef96912e7c93a7b301
Nani-Sore-0 <urke.radule@gmail.com>
2018-10-20 16:21:43 +0200

    loader

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-svg4everybody()
+$(window).on('load', function(){
+    $(".loader").fadeOut()
+})
 
+svg4everybody()
 $(document).ready(function () {
     new SmoothScroll('.link-ani');
     AOS.init({ duration: 1000 });
@@ -116,54 +119,7 @@ $(document).ready(function () {
         scrollSpeed: 750,
         filter: ':not(.external)'
     });
-});
-
-
-// $(document).ready(function () {
-//     new SmoothScroll('.link-ani')
-//     AOS.init({duration: 1000})
-
-//     $('.about__slideshow').slick({
-//         autoplay: true,
-//         arrows: false,
-//         autoplaySpeed: 2000,
-//     })
-//     $('.menu__box').tabslet({
-//         animation: true,
-//     })
-//     $('.gallery__box').tabslet({
-//         animation: true,
-//     })
-//     $('.wine__box').tabslet({
-//         animation: true,
-//     })
-
-//     function clearGallery(cur) {
-//         document.getElementById(cur).innerHTML = ''
-//     }
-//     function createGallery(cur) {
-//         // How many images in each section?
-//         var imgNum
-//         switch (cur) {
-//             case 'interior':
-//                 imgNum = 33
-//                 break;
-//             case 'garden':
-//                 imgNum = 16
-//                 break;
-//             case 'events':
-//                 imgNum = 77
-//                 break;
-//             case 'food':
-//                 imgNum = 41
-//                 break;
-//             case 'deserts':
-//                 imgNum = 14
-//                 break;
-//             case 'details':
-//                 imgNum = 28
-//                 break;
-//         }
+})
 
 //         var galleryTemplate = 
 //         `
@@ -176,67 +132,3 @@ $(document).ready(function () {
 //             </div>
 //         </div>
 //         `
-//         document.getElementById(cur).innerHTML = galleryTemplate
-//     }
-//     function showImages(cur) {
-//         $(`.gallery__${cur}-selected`).slick({
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             fade: true,
-//             asNavFor: `.gallery__${cur}-view`,
-//             adaptiveHeight: true,
-//             prevArrow: '<a class="gallery__prev"><svg class="gallery__icon"><use xlink:href="img/sprite.svg#icon-chevron-left"></use></svg></a>',
-//             nextArrow: '<a class="gallery__next"><svg class="gallery__icon"><use xlink:href="img/sprite.svg#icon-chevron-right"></use></svg></a>',
-//         })
-//         $(`.gallery__${cur}-view`).slick({
-//             slidesToShow: 4,
-//             slidesToScroll: 1,
-//             asNavFor: `.gallery__${cur}-selected`,
-//             centerMode: true,
-//             focusOnSelect: true,
-//             arrows: false,
-//             responsive: [
-//                 {
-//                   breakpoint: 600,
-//                   settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1
-//                   }
-//                 }
-//             ]
-//         })
-//     }
-//     function activateGallery(cur) {
-//         clearGallery(cur)
-//         createGallery(cur)
-//         showImages(cur)
-//     }
-//     var posible = ['interior', 'garden', 'events', 'food', 'deserts', 'details']
-//     document.querySelector('.gallery__box').addEventListener('click', event => {
-//         if (posible.includes(event.target.innerHTML)) {
-//             activateGallery(event.target.innerHTML)
-//         }
-//     }, true)
-
-//     //Fix gallery bug
-//     posible.forEach(cur => activateGallery(cur))
-//     activateGallery('interior')
-
-
-//     var tabsAnimation = ['.gallery__tabs', '.menu__tabs', '.wine__tabs']
-//     tabsAnimation.forEach(cur => document.querySelector(cur).addEventListener('click', changeTab, true))
-//     function changeTab(event) {
-//         if (event.target.className === 'gallery__tab' || event.target.className === 'menu__tab' || event.target.className === 'wine__tab') {
-//             $(`.${event.currentTarget.className} li .activeTabView`).removeClass('activeTabView')
-//             $(event.target).addClass('activeTabView')
-//         }
-//     }
-
-
-//     $('.nav').onePageNav({
-//         currentClass: 'nav__current',
-//         changeHash: false,
-//         scrollSpeed: 750,
-//         filter: ':not(.external)',
-//     })
-// })
